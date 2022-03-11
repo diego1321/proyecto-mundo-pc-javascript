f3a4440bbe2b8c5955d25323a6e81cac860cd350
diego <diegoalejandrop574@gmail.com>
2022-03-10 23:16:20 -0500

    terminado

--- leccion12/proyecto-sistema-de-ventas.js
+++ leccion12/proyecto-sistema-de-ventas.js
@@ -6,49 +6,94 @@ class Producto{
         this._nombre = nombre;
         this._precio = precio;
     }
+
     get idProducto(){
         return this._idProducto;
     }
+
     get nombre(){
         return this._nombre;
     }
+
     set nombre(nombre){
         this._nombre = nombre;
     }
+
     get precio(){
         return this._precio;
     }
+    
     set precio(precio){
-        this._precio = precio;
+        return this._precio = precio;
     }
+
     toString(){
-        return `${this._idProducto} ${this._nombre} ${this._precio}`;
+        return `idProducto: ${this._idProducto}, nombre: ${this._nombre}, precio: $${this._precio}  `;
     }
 }
-
 
 class Orden{
     static contadorOrdenes = 0;
-    static MAXProducto = 5;
+
+    static get MAX_PRODUCTOS(){
+        return 5;
+    } 
+
     constructor(){
         this._idOrden = ++Orden.contadorOrdenes;
-        let Producto = ['pan', 'queso'];
-        contadorProductosAgregados = 0;
-        const MAXProducto = 5;
+        this._productos = [];
+        //this._contadorProductosAgregados = 0;
     }
-   agregarProducto(){
-    return this._nombre + ' ' + this ._precio + this.contadorProductosAgregados
-   }
-   calcularTotal(){
-       return this._idProducto + ' ' + this._idOrden;
-   }
-   toString(){
-       return this.calcularTotal;
-   }
+
+    get idOrden(){
+        return this._idOrden;
+    }
+
+    agregarProducto(producto){
+        if( this._productos.length < Orden.MAX_PRODUCTOS){
+            this._productos.push(producto);
+            //this._productos[this._contadorProductosAgregados++] = producto;
+        }
+        else{
+            console.log('No se pueden agregar más productos');
+        }
+    }
+
+    calcularTotal(){
+        let totalVenta = 0;
+        for(let producto of this._productos){
+            totalVenta += producto.precio;//totalVenta = totalVenta + producto.precio
+        }
+        return totalVenta;
+    }
+
+    mostrarOrden(){
+        let productosOrden = '';
+        for( let producto of this._productos){
+            productosOrden += '\n{' + producto.toString() + '}';
+        }
+
+        console.log(`Orden: ${this._idOrden} Total: $${this.calcularTotal()}, Productos: ${productosOrden} `);
+    }
+
 }
 
 
 
+let producto1 = new Producto('Pantalón', 200);
+let producto2 = new Producto('Camisa', 100);
 
+let orden1 = new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden1.mostrarOrden();
 
-
+let orden2 = new Orden();
+let producto3 = new Producto('Cinturon', 50);
+orden2.agregarProducto(producto3);
+orden2.agregarProducto(producto1);
+orden2.agregarProducto(producto2);
+orden2.agregarProducto(producto3);
+orden2.agregarProducto(producto1);
+orden2.agregarProducto(producto2);
+orden2.mostrarOrden();
